d2102f89cd4d11968d65c171a61df7a6ee6c0538
secon <seconoid@gmail.com>
2015-03-26 01:30:17 +0900

    add fadeIn function
    ページ全体がフェードインする機能を追加

--- js/common.js
+++ js/common.js
@@ -1,4 +1,15 @@
-$(window).on('load resize', function(){
+// ロード時処理
+$(document).ready(function(){
+  // ページ全体をフェードイン
+  $('body').fadeIn(1000);
+
+  // 正方形を生成
+  var w = $('.square').width();
+  $('.square').css('height', w);
+});
+
+// リサイズ時に正方形を維持
+$(window).on('resize', function(){
   var w = $('.square').width();
   $('.square').css('height', w);
 });
