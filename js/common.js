01a67da735406ed35421cc7e540f4b8d955f401e
secon <seconsecond@gmail,com>
2015-04-06 23:15:31 +0900

    change font and header
    フォントとヘッダ部分の変更

--- js/common.js
+++ js/common.js
@@ -4,12 +4,21 @@ $(document).ready(function(){
   $('body').fadeIn(1000);
 
   // 正方形を生成
-  var w = $('.square').width();
-  $('.square').css('height', w);
+  var w = $('.box-b').width();
+  $('.box-b').css('height', w);
+  var w = $('.box-c').width();
+  $('.box-c').css('height', w);
+  var w = $('.box-d').width();
+  $('.box-d').css('height', w);
+  $('.box-d').css('line-height', w+'px');
 });
 
 // リサイズ時に正方形を維持
 $(window).on('resize', function(){
-  var w = $('.square').width();
-  $('.square').css('height', w);
+  var w = $('.box-b').width();
+  $('.box-b').css('height', w);
+  var w = $('.box-c').width();
+  $('.box-c').css('height', w);
+  var w = $('.box-d').width();
+  $('.box-d').css('height', w);
 });
