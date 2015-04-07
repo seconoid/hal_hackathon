bad6b716eeb2be6aa20dd91ecb47712bc8e4b43a
secon <seconoid@gmail.com>
2015-04-07 16:23:04 +0900

    add contentsCange function
    コンテンツがクリックされた時にメイン表示領域の内容が変更される機能を追加

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   // ページ全体をフェードイン
   $('body').fadeIn(1000);
+  $('.sub').hide();
 
   // 正方形を生成
   var w = $('.box-b').width();
@@ -21,4 +22,49 @@ $(window).on('resize', function(){
   $('.box-c').css('height', w);
   var w = $('.box-d').width();
   $('.box-d').css('height', w);
+});
+
+// コンテンツをクリックでメイン領域の表示を変更
+$('.about-trg').click(function(){
+  $('.main').hide();
+  $('.main').addClass('sub');
+  $('.main').removeClass('main');
+  $('.about').addClass('main');
+  $('.about').removeClass('.sub');
+  $('.main').show();
+  $('.about-trg').removeClass('off');
+  $('.about-trg').addClass('on');
+});
+
+$('.concept-trg').click(function(){
+  $('.main').hide();
+  $('.main').addClass('sub');
+  $('.main').removeClass('main');
+  $('.concept').addClass('main');
+  $('.concept').removeClass('.sub');
+  $('.main').show();
+  $('.concept-trg').removeClass('off');
+  $('.concept-trg').addClass('on');
+});
+
+$('.archieve-trg').click(function(){
+  $('.main').hide();
+  $('.main').addClass('sub');
+  $('.main').removeClass('main');
+  $('.archieve').addClass('main');
+  $('.archieve').removeClass('.sub');
+  $('.main').show();
+  $('.archieve-trg').removeClass('off');
+  $('.archieve-trg').addClass('on');
+});
+
+$('.news-trg').click(function(){
+  $('.main').hide();
+  $('.main').addClass('sub');
+  $('.main').removeClass('main');
+  $('.news').addClass('main');
+  $('.news').removeClass('.sub');
+  $('.main').show();
+  $('.news-trg').removeClass('off');
+  $('.news-trg').addClass('on');
 });
